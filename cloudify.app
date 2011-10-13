f6b91a768a726644be0fd6178c7aef2eda377636
Eric Jennings <ericj@loopshot.com>
2011-10-12 19:38:27 -0700

    initial import for cloudify

---
+++ cloudify.app
@@ -0,0 +1,11 @@
+{
+    "title":"Cloudify",
+    "desc":"View tag and word clouds of your data",
+    "handle":"cloudify",
+    "author":"erictj",
+    "viewer": "links",
+    "uses":["facebook", "twitter"],
+    "status":"stable",
+    "static":"true",
+    "autoInstall":true
+}
--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,10 @@
 var baseUrl = '';
 
 $(function() {
-    $.getJSON(baseUrl + '/query/getPhoto', {'sort':sort}, function(data) {
-        // display your data!
+    $.getJSON(baseUrl + '/Me/links?limit=500?full=1', {'sort':sort}, function(data) {
+        for (var record in data) {
+            console.log(record.encounters.via.entities.hashtags);
+        }
     });
 });
 
